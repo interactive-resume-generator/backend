da658112c31c73d70223572d2f4952726701d817
Ethan Albers <ekalbers22@gmail.com>
2023-05-25 07:57:17 -0500

    added auth0

--- interactive_resume_api/urls.py
+++ interactive_resume_api/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/resumes/', include('api.urls')),
+    path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("callback", views.callback, name="callback"),
 ]
